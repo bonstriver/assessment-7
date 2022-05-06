564ff11fd18c52bd24e0947cada02e36536380c7
Danny Martinez <102090899+bonstriver@users.noreply.github.com>
2022-05-06 15:14:27 -0700

    Update stepTwo.js
    Added changes upon review

--- stepTwo.js
+++ stepTwo.js
@@ -1,20 +1,27 @@
-const sumZero = (arr) => {
+// Write a function that takes in an array of numbers. The function should return True if any two numbers in list sum to 0, and false otherwise.
+
+const sumZero = arr => {
     let solution = false
     for(let i = 0; i < arr.length; i++){
-        for(let k = i+1; k < arr.length; k++){
-        if(arr[i] + arr[k] === 0){
-            solution = true;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                solution = true;
             }
         }
     }  
     return solution
 }
+
 console.log(sumZero([1,2,3,-2]))
 // I believe the runtime of this code would be O(n^2)
 
-const hasUniqueChars = (str) => {
+// ----------------------------------------------------
+
+// Write a function that takes in a single word, as a string. It should return True if that word contains only unique characters. Return False otherwise.
+
+const hasUniqueChars = str => {
     for(let i = 0; i <= str.length; i++){
-        for(let j = i+1; j <= str.length; j++){
+        for(let j = i + 1; j < str.length; j++){
             if(str[i] === str[j]){
                 return false
             }
@@ -22,16 +29,29 @@ const hasUniqueChars = (str) => {
     }
     return true
 }
-console.log(hasUniqueChars("Carrot"))
+
+console.log(hasUniqueChars("dermatoglyphics"))
 // I believe the runtime of this code would be O(n^2)
 
-const pangram = (str) => {
-    let regex = /([a-z])(?!.*\1)/gi;
-    return (str.match(regex) || []).length === 26
-    }
-console.log(pangram("I like cats, but not mice"))
+// ----------------------------------------------------
+
+// A pangram is a sentence that contains all the letters of the English alphabet at least once, like “The quick brown fox jumps over the lazy dog.”
+
+// Write a function to check a sentence to see if it is a pangram or not.
+
+const isPangram = (str) => {
+    let lowerStr = str.toLowerCase();
+    let letterCheck = lowerStr.match(/[a-z]/g);
+    letterCheck = [...new Set(letterCheck)];
+    return letterCheck.length === 26;
+};
+
+console.log(isPangram("abcdefghijklmnopqrstuvwxyz"))
 // I believe the runtime of this code would be O(n)
 
+// ----------------------------------------------------
+
+// Write a function, find_longest_word, that takes a list of words and returns the length of the longest one.
 
 const findLongestWord = (arr) => {
     longestWord = 0
@@ -42,5 +62,6 @@ const findLongestWord = (arr) => {
     }
     return longestWord
 }
+
 console.log(findLongestWord(["long", "longer", "longest"]))
 // I believe the runtime of this code would be O(n)
